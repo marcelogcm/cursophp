5f6df99fbe4492d4f018c88cf92ccbddd00e101f
marcelogcm <61303161+marcelogcm@users.noreply.github.com>
2020-02-21 02:44:03 -0300

    nova alteração
    teste

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-echo "<strong>Olá Mundo456!!!!</strong>";
-
-echo "eita bixiga"
-=======
+<
 echo "<strong>Olá Mundo 1223!!!!</strong>";
 echo "Olá Marcelo Giovanni";
->>>>>>> 34a1ec2f071aa203df218469294784cc4391f940
 
 ?>
